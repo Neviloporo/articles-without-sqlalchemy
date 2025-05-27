030112dac263a2f7c890e8b27ae882e8a4398a70
nevil <nevil.oporo@student.moringaschool.com>
2025-05-27 08:56:09 +0300

    added a magazine

---
+++ code-challenge/lib/models/magazine.py
@@ -0,0 +1,62 @@
+from lib.db.connection import get_connection
+from lib.models.articles import Article
+from lib.models.author import Author
+
+class Magazine:
+    def __init__(self, id=None, name=None, category=None):
+        self.id = id
+        self.name = name
+        self.category = category
+
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO magazines (name, category) VALUES (?, ?)", (self.name, self.category))
+        self.id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    def find_by_id(cls, id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM magazines WHERE id = ?", (id,))
+        row = cursor.fetchone()
+        conn.close()
+        return cls(*row) if row else None
+
+    def articles(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE magazine_id = ?", (self.id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [Article(*row) for row in rows]
+
+    def contributors(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT DISTINCT a.* FROM authors a
+        JOIN articles ar ON ar.author_id = a.id
+        WHERE ar.magazine_id = ?
+        """, (self.id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [Author(*row) for row in rows]
+
+    def article_titles(self):
+        return [article.title for article in self.articles()]
+
+    def contributing_authors(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT author_id, COUNT(*) as article_count FROM articles
+        WHERE magazine_id = ?
+        GROUP BY author_id
+        HAVING article_count > 2
+        """, (self.id,))
+        author_ids = [row[0] for row in cursor.fetchall()]
+        conn.close()
+        return [Author.find_by_id(id) for id in author_ids]
